af8f0f574a30e64376ce84d5e4a66ffd71ca7c24
Jyothika <jyothisigirisetty@gmail.com>
2026-02-07 18:39:40 +0530

    data_loader

--- SRC/data_loader.py
+++ SRC/data_loader.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 def load_data():
-    df = pd.read_csv('/content/drive/MyDrive/AI Final Project/NetflixSimple.csv')
+    df = pd.read_csv('data/NetflixSimple.csv')
     df = df.fillna('')
     return df
