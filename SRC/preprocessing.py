910f382d1db4e1679056e60ccb06629ca7552285
Jyothika <jyothisigirisetty@gmail.com>
2026-02-07 16:44:13 +0530

    preprocessing

--- SRC/preprocessing.py
+++ SRC/preprocessing.py
@@ -3,9 +3,10 @@ import nltk
 from nltk.corpus import stopwords
 
 nltk.download('stopwords')
+
 stop_words = set(stopwords.words('english'))
 
-def clean_text(text):
+def normalize_text(text):
     text = text.lower()
     text = re.sub(r'[^a-zA-Z ]', ' ', text)
     tokens = text.split()
