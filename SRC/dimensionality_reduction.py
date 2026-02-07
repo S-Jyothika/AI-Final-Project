f831d86f07a07246a073cacfb148eaed30bd03d4
Jyothika <jyothisigirisetty@gmail.com>
2026-02-07 16:58:58 +0530

    dimensionality_reduction

--- SRC/dimensionality_reduction.py
+++ SRC/dimensionality_reduction.py
@@ -1,6 +1,6 @@
 from sklearn.decomposition import TruncatedSVD
 
-def reduce_dimensions(X):
-    svd = TruncatedSVD(n_components=200, random_state=42)
+def apply_svd(X, n_components=200):
+    svd = TruncatedSVD(n_components=n_components, random_state=42)
     X_reduced = svd.fit_transform(X)
     return X_reduced, svd
