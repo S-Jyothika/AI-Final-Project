6cc628c4e9a224dde1f89b3eb30869ad6ae9392b
Jyothika <jyothisigirisetty@gmail.com>
2026-02-07 16:59:36 +0530

    clustering

--- SRC/clustering.py
+++ SRC/clustering.py
@@ -1,6 +1,6 @@
 from sklearn.cluster import KMeans
 
-def perform_clustering(X_reduced):
-    kmeans = KMeans(n_clusters=10, random_state=42)
-    cluster_labels = kmeans.fit_predict(X_reduced)
-    return cluster_labels, kmeans
+def apply_kmeans(X_reduced, k=10):
+    kmeans = KMeans(n_clusters=k, random_state=42)
+    labels = kmeans.fit_predict(X_reduced)
+    return labels, kmeans
